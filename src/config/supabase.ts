7624aecbab342f4bf2ed55244e4eb9cbd480b199
Kamesh Sangili <kamesh.sangili@gmail.com>
2025-04-03 11:37:00 -0700

    Delete src/config/supabase.ts

--- src/config/supabase.ts
+++
@@ -1,7 +0,0 @@
-
-// This file contains sensitive information and should not be committed to version control
-// Create a supabase.example.ts file with empty values as a template
-export const SUPABASE_CONFIG = {
-  url: "https://vzdystnqpizcrdryaqdq.supabase.co",
-  publishableKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ6ZHlzdG5xcGl6Y3JkcnlhcWRxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM0Njg1NjcsImV4cCI6MjA1OTA0NDU2N30.AgWYphKv2lSLDeIBHk6uPm3-9-NkjWyxpB8hHGAZwBw"
-};
