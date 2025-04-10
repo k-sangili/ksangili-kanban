a788e576fca953e66d048d82c50a7b528017eed1
Kamesh Sangili <kamesh.sangili@gmail.com>
2025-04-10 13:13:40 -0700

    Create supabase.ts

---
+++ src/config/supabase.ts
@@ -0,0 +1,6 @@
+
+// Configuration for Supabase
+export const SUPABASE_CONFIG = {
+  url: "https://vzdystnqpizcrdryaqdq.supabase.co",
+  publishableKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ6ZHlzdG5xcGl6Y3JkcnlhcWRxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM0Njg1NjcsImV4cCI6MjA1OTA0NDU2N30.AgWYphKv2lSLDeIBHk6uPm3-9-NkjWyxpB8hHGAZwBw"
+};
